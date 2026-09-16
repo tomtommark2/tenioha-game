d4a671f7fc90305e6798fd0c6b36d644489cdb07
bracon <tomtom.mark2@gmail.com>
2026-09-16 23:03:05 +0900

    Add contact email link to feedback board notice

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Universal Global Scope (Browser & Service Worker)
 (function (global) {
-    global.GAME_VERSION = "2026.0916.1322";
+    global.GAME_VERSION = "2026.0916.2258";
 })(typeof window !== 'undefined' ? window : self);
