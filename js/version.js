1ff5343b3bc7215d3bffbab16348e1a39be6343e
bracon <tomtom.mark2@gmail.com>
2026-09-24 17:05:37 +0900

    Use smooth rendering for vocabulary illustrations

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 // Universal Global Scope (Browser & Service Worker)
 (function (global) {
-    global.GAME_VERSION = "2026.0923.2059";
+    global.GAME_VERSION = "2026.0924.1704";
 })(typeof window !== 'undefined' ? window : self);
